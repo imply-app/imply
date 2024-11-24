0a1ab0e2f47c83fc0c43bd71c05213c29df7b8ea
Tri Nguyen Van <nguyenvantri2820@gmail.com>
2024-11-24 20:57:28 +0100

    feat: add og image

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -9,6 +9,40 @@ export default createHandler(() => (
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.svg" />
+
+          <title>Imply | All-in-one team platform</title>
+          <meta name="description" content="All-in-one team platform" />
+
+          <meta
+            property="og:title"
+            content="Imply | All-in-one team platform"
+          />
+          <meta property="og:description" content="All-in-one team platform" />
+          <meta property="og:image" content="https://imply.app/banner.svg" />
+          <meta property="og:image:alt" content="All-in-one team platform" />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="600" />
+          <meta property="og:site_name" content="Imply" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://imply.app" />
+          <meta
+            property="og:image:secure_url"
+            content="https://imply.app/banner.svg"
+          />
+          <meta property="og:image:type" content="image/png" />
+          <meta property="og:locale" content="en_US" />
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta
+            name="twitter:title"
+            content="Imply | All-in-one team platform"
+          />
+          <meta name="twitter:description" content="All-in-one team platform" />
+          <meta name="twitter:image" content="https://imply.app/banner.svg" />
+          <meta name="twitter:image:alt" content="All-in-one team platform" />
+          <meta name="twitter:site" content="@tri2820" />
+          <meta name="twitter:creator" content="@tri2820" />
+
           {assets}
         </head>
         <body>
